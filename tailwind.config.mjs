222bde5b0e42a8444e7d0f3b9227a3a9326ea590
josephstakeland <josephstakeland@gmail.com>
2024-06-17 19:50:06 -0500

    Added Section 5

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,57 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
+
+			keyframes: {
+                "fade-in": {
+                    "0%": {
+                        opacity: 0
+                    },
+                    "100%": {
+                        opacity: 1
+                    },
+                },
+
+				'tada': {
+                    "0%": {
+                        transform: "scale3d(1, 1, 1)",
+                    },
+                    "10%, 20%": {
+                        transform: "scale3d(0.9, 0.9, 0.9) rotate3d(0, 0, 1, -3deg)",
+                    },
+                    "30%, 50%, 70%, 90%": {
+                        transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, 3deg)",
+                    },
+                    "40%, 60%, 80%": {
+                        transform: "scale3d(1.1, 1.1, 1.1) rotate3d(0, 0, 1, -3deg)",
+                    },
+                    "100%": {
+                        transform: "scale3d(1, 1, 1)",
+                    },
+				},
+
+				"drop-in": {
+                    "0%": {
+                        opacity: "0",
+                        transform: "scale(0)",
+                        animationTimingFunction: "cubic-bezier(0.34, 1.61, 0.7, 1)",
+                    },
+                    "100%": {
+                        opacity: "1",
+                        transform: "scale(1)",
+                    },
+                },
+	
+			},
+
+			animation: {
+				"fade-in": "fade-in 1.5s ease-in-out infinite",
+				'tada': 'tada 1s ease-in-out',
+				'drop-in': 'drop-in 1s ease-in-out 0.25s 1',
+				'spin-slow': 'spin 4s linear infinite',
+				'bounce-slow': 'bounce 4s linear infinite',
+			},
+
 			screens: {
 				sm: '388px',
 				md: '768px',
@@ -22,5 +73,9 @@ export default {
             } 
 		},
 	},
+
+	
 	plugins: [],
 }
+
+
